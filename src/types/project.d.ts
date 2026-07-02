5b6515e98e8fc338da773c7b682b18bd91c89663
SIE-QIUQINGSHENG <2586597832@qq.com>
2026-07-02 18:11:14 +0800

    feat: 类型定义新增 backupBasePath 字段

--- src/types/project.d.ts
+++ src/types/project.d.ts
@@ -15,6 +15,7 @@ declare type RowProjectType = {
 	description: string | null;
 	isDefault: number | null;
 	assemblyOutPath: string | null;
+	backupBasePath: string | null;
 };
 
 interface ProjectTableType extends TableType<GetProjectTableParams> {
